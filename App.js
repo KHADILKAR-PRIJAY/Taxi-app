757460a6af44bfa0c288d750324281eced4d0cb1
KHADILKAR-PRIJAY <deven.khadilkar@gmail.com>
2022-10-13 23:59:36 -0400

    initial commit

---
+++ App.js
@@ -0,0 +1,45 @@
+
+import React, { useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  FlatList,
+} from 'react-native';
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Signup from './Screens/Signup';
+
+
+
+
+const Stack = createStackNavigator();
+const MyTheme = {
+  dark: false,
+  colors: {
+    primary: 'rgb(255, 45, 85)',
+    background: 'rgb(242, 242, 242)',
+    card: 'rgb(255, 255, 255)',
+    text: 'rgb(28, 28, 30)',
+    border: 'rgb(199, 199, 204)',
+    notification: 'rgb(255, 69, 58)',
+  },
+};
+
+export default function App() {
+  return (
+    <NavigationContainer theme={MyTheme}>
+      <Stack.Navigator>
+  
+        <Stack.Screen
+          name="Signup"
+          component={Signup}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
