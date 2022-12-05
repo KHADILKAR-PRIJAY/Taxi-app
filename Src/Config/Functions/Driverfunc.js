c2561ae820d3088752b09992a5d37089f12b2943
Rutulmalvaniya <rutul.malvaniya2000@gmail.com>
2022-12-05 18:07:04 -0500

    Driver request functions

--- Src/Config/Functions/Driverfunc.js
+++ Src/Config/Functions/Driverfunc.js
@@ -87,4 +87,127 @@ let DeletePost = (uid) => {
         });
     });
   };
-  export { Post_Trip_Func, get_MY_Trip_Posts, DeletePost };
+
+  let get_All_Pending_Requests = (id) => {
+    return new Promise((resolve, reject) => {
+      let Requests = [];
+      firebase
+        .firestore()
+        .collection("Trip_Posts")
+        .doc(id)
+        .collection("bookseats")
+        .where("Status", "==", "Pending")
+        .onSnapshot((snap) => {
+          if (!snap.empty) {
+            snap.forEach((doc) => {
+              let Request = doc.data();
+              Request.rid = doc.id;
+              Requests.push(Request);
+              console.log(Request, "pending");
+              resolve(Requests);
+            });
+          } else {
+            resolve([]);
+          }
+        });
+    });
+  };
+  
+  let get_All_Rejected_Requests = (id) => {
+    return new Promise((resolve, reject) => {
+      let Requests = [];
+      firebase
+        .firestore()
+        .collection("Trip_Posts")
+        .doc(id)
+        .collection("bookseats")
+        .where("Status", "==", "Rejected")
+        .onSnapshot((snap) => {
+          snap.forEach((doc) => {
+            
+            let Request = doc.data();
+            Request.rid = doc.id;
+            Requests.push(Request);
+            console.log(Request, "Rejected");
+            resolve(Requests);
+          });
+        });
+    });
+  };
+  
+  let get_All_Accepted_Requests = (id) => {
+    return new Promise((resolve, reject) => {
+      try {
+        let Requests = [];
+  
+        firebase
+          .firestore()
+          .collection("Trip_Posts")
+          .doc(id)
+          .collection("bookseats")
+          .where("Status", "==", "Accepted")
+          .onSnapshot((snap) => {
+            if (!snap.empty) {
+              snap.forEach((doc) => {
+                let Request = doc.data();
+                Request.rid = doc.id;
+                Requests.push(Request);
+                console.log(Request, "Accepted");
+                resolve(Requests);
+              });
+            } else {
+              resolve([]);
+            }
+          });
+      } catch (error) {
+        alert(error);
+      }
+    });
+  };
+  
+  let AcceptRequest = (id, rid) => {
+    return new Promise((resolve, reject) => {
+      try {
+        firebase
+          .firestore()
+          .collection("Trip_Posts")
+          .doc(id)
+          .collection("bookseats")
+          .doc(rid)
+          .update({
+            Status: "Accepted",
+          });
+      } catch (error) {
+        alert(error);
+      }
+    });
+  };
+  
+  let RejectRequest = (id, rid) => {
+    return new Promise((resolve, reject) => {
+      try {
+        firebase
+          .firestore()
+          .collection("Trip_Posts")
+          .doc(id)
+          .collection("bookseats")
+          .doc(rid)
+          .update({
+            Status: "Rejected",
+          });
+      } catch (error) {
+        alert(error);
+      }
+    });
+  };
+  export {
+    Post_Trip_Func,
+    get_MY_Trip_Posts,
+    DeletePost,
+    get_All_Trip_Posts,
+    get_All_Pending_Requests,
+    get_All_Rejected_Requests,
+    get_All_Accepted_Requests,
+    AcceptRequest,
+    RejectRequest,
+  };
